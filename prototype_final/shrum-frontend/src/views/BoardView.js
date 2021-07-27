91c4a5ec7a97388619bcdda05040b13179bbfe53
黄永丽 <yongli.huang@tum.de>
2021-07-27 11:23:41 +0200

    LS works

--- prototype_final/shrum-frontend/src/views/BoardView.js
+++ prototype_final/shrum-frontend/src/views/BoardView.js
@@ -13,57 +13,56 @@ import Board from 'react-trello'
  * @param {props} props
  */
 
-//  class NewCardForm extends Component {
-//   handleAdd = () => this.props.onAdd({title: this.titleRef.value, description: this.descRef.value, label: this.label.value, assignee: this.assignee.value, priority: this.priority.value, reviewer: this.reviewer.value})
-//   setTitleRef = (ref) => this.titleRef = ref
-//   setDescRef = (ref) => this.descRef = ref
-//   setLabelRef = (ref) => this.label = ref
-//   setAssigneeRef = (ref) => this.assignee = ref
-//   setPriorityRef =(ref) => this.priority = ref
-//   setReviewerRef = (ref) => this.reviewer = ref
-//   render() {
-//   const {onCancel} = this.props
-//   return (
-//    <div style={{background: 'white', borderRadius: 3, border: '1px solid #eee', borderBottom: '1px solid #ccc'}}>
-//     <div style={{padding: 5, margin: 5}}>
-//       <div>
-//         <div style={{marginBottom: 5}}>
-//           <input type="text" ref={this.setTitleRef} placeholder="User" />
-//         </div>
-//         <div style={{marginBottom: 5}}>
-//           <input type="text" ref={this.setDescRef} placeholder="Task Description" />
-//         </div>
-//         <div style={{marginBottom: 5}}>
-//           <input type="text" ref={this.setLabelRef} placeholder="points" />
-//         </div>
-//         <div style={{marginBottom: 5}}>
-//           <input type="text" ref={this.setAssigneeRef} placeholder="assignee" />
-//         </div>
-//         <div style={{marginBottom: 5}}>
-//           <input type="text" ref={this.setPriorityRef} placeholder="priority" />
-//         </div>
-//         <div style={{marginBottom: 5}}>
-//           <input type="text" ref={this.setReviewerRef} placeholder="reviewer" />
-//         </div>
-//       </div>
-//       <button onClick={this.handleAdd}>Add</button>
-//       <button onClick={onCancel}>Cancel</button>
-//     </div>
-//   </div>
-//     ) 
-//   }
-// }
+ class NewCardForm extends Component {
+  handleAdd = () => this.props.onAdd({title: this.titleRef.value, description: this.descRef.value, label: this.label.value, assignee: this.assignee.value, priority: this.priority.value, reviewer: this.reviewer.value})
+  setTitleRef = (ref) => this.titleRef = ref
+  setDescRef = (ref) => this.descRef = ref
+  setLabelRef = (ref) => this.label = ref
+  setAssigneeRef = (ref) => this.assignee = ref
+  setPriorityRef =(ref) => this.priority = ref
+  setReviewerRef = (ref) => this.reviewer = ref
+  render() {
+  const {onCancel} = this.props
+  return (
+   <div style={{background: 'white', borderRadius: 3, border: '1px solid #eee', borderBottom: '1px solid #ccc'}}>
+    <div style={{padding: 5, margin: 5}}>
+      <div>
+        <div style={{marginBottom: 5}}>
+          <input type="text" ref={this.setTitleRef} placeholder="User" />
+        </div>
+        <div style={{marginBottom: 5}}>
+          <input type="text" ref={this.setDescRef} placeholder="Task Description" />
+        </div>
+        <div style={{marginBottom: 5}}>
+          <input type="text" ref={this.setLabelRef} placeholder="points" />
+        </div>
+        <div style={{marginBottom: 5}}>
+          <input type="text" ref={this.setAssigneeRef} placeholder="assignee" />
+        </div>
+        <div style={{marginBottom: 5}}>
+          <input type="text" ref={this.setPriorityRef} placeholder="priority" />
+        </div>
+        <div style={{marginBottom: 5}}>
+          <input type="text" ref={this.setReviewerRef} placeholder="reviewer" />
+        </div>
+      </div>
+      <button onClick={this.handleAdd}>Add</button>
+      <button onClick={onCancel}>Cancel</button>
+    </div>
+  </div>
+    ) 
+  }
+}
 
 
 
 function BoardView() {
-  //const [retrievedObject, setRetrievedObject] = useState([]);
-  
-    const data = {
+    let data = {
       lanes: [
       {
         id: 'lane1',
         title: 'To Do',
+        cards:[],
         cards: [
           {id: 'Card1', title: 'Assignment 1', description: 'Deadline: Sunday, May 2nd.',assignee: 'None', priority:'low ❗', reviewer: 'None', label: '3 Points', draggable: true},
           {id: 'Card2', title: 'Assignment 2', description: 'Deadline: Sunday, May 16th.', assignee: 'None', priority:'low ❗', reviewer: 'None',  label: '5 Points', metadata: {sha: 'be312a1'}},
@@ -92,37 +91,30 @@ function BoardView() {
     ]
   };
 
-
-
-
-
-  localStorage.setItem('data', JSON.stringify(data));
-  var retrievedObject = localStorage.getItem('data');
-  console.log('data: ', JSON.parse(retrievedObject));
-
-  
-  
-
+  //localStorage.setItem('data', JSON.stringify(data));
+  //var retrievedObject = localStorage.getItem('data') || [];
+  //console.log('old data: ', JSON.parse(retrievedObject));
 
     return (
       <Board style = {{backgroundColor: 'grey'}}
       editable
-       onDataChange={(newData) => {localStorage.setItem('data', JSON.stringify(newData));
-          retrievedObject = localStorage.getItem('data');
-          console.log("data changed",JSON.parse(retrievedObject))
-      }}
-       onCardAdd={(card) => {localStorage.setItem('data', JSON.stringify(card));
-                          retrievedObject = localStorage.getItem('data');
+    
+      onCardAdd={(card) => {localStorage.setItem('data', JSON.stringify(card));
+                           var retrievedObject = localStorage.getItem('data');
                          console.log("new card: ", retrievedObject)}}
-                         cardDraggable
-                         draggable
-                         data={JSON.parse(retrievedObject)}
-                         
-                         collapsibleLanes
-                         //components={{NewCardForm: NewCardForm}} 
+
+      onDataChange={(newData) => {
+                          localStorage.clear();
+                          localStorage.setItem('data', JSON.stringify(newData));
+                          var retrievedObject = localStorage.getItem('data');
+                          console.log("data changed",JSON.parse(retrievedObject))
+                     }}
+                    cardDraggable
+                    draggable
+                    data={JSON.parse(localStorage.getItem('data'))  || data }   
+                    collapsibleLanes
+                    //components={{NewCardForm: NewCardForm}} 
                          />
-                        
-                         
                          
     );
 
